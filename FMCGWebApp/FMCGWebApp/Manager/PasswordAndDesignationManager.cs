4e2e56092e0265aa32a31e69da5a7ecd5bfcbcf1
Nora Alam <nora.alam07@gmail.com>
2018-05-30 02:48:52 +0600

    add all manager

---
+++ FMCGWebApp/FMCGWebApp/Manager/PasswordAndDesignationManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FMCGWebApp.Gateway;
+using FMCGWebApp.Models;
+
+namespace FMCGWebApp.Manager
+{
+    public class PasswordAndDesignationManager
+    {
+        private PasswordAndDesignationGateway _passwordAndDesignation = new PasswordAndDesignationGateway();
+        private ShopInfoGateway _shopInfoGateway = new ShopInfoGateway();
+        public List<Employee> ListOfEmployee()
+        {
+            return _shopInfoGateway.GetAllEmployees();
+        }
+
+        public List<UserType> GetUserType()
+        {
+            return _passwordAndDesignation.GetUserType();
+        }
+
+        public bool IsPasswordSet(EmployeePassword employee)
+        {
+            return _passwordAndDesignation.IsPasswordSet(employee);
+        }
+
+        public int SetEmployeePassword(EmployeePassword employee)
+        {
+            return _passwordAndDesignation.SetEmployeePassword(employee);
+        }
+
+        public List<Employee> GetEmployeeById(int id)
+        {
+            return _passwordAndDesignation.GetEmployeeById(id);
+        }
+
+        public bool IsUserRoleSet(EmployeeUserType employee)
+        {
+            return _passwordAndDesignation.IsUserRoleSet(employee);
+        }
+
+        public int SetEmployeeUserType(EmployeeUserType employee)
+        {
+            return _passwordAndDesignation.SetEmployeeUserType(employee);
+        }
+    }
+}
